5ebe054c8ac9819776bf64aa7104ed3803ab347a
wenbo <3378814052@qq.com>
2023-12-29 16:35:22 +0800

    feat: main func

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+use std::sync::Mutex;
+use crate::dao::db_context::Database;
+use std::sync::Arc;
 pub mod config;
+pub mod model;
+pub mod dao;
+
+// AppState
+// This the primary dependency for our application's dependency injection.
+// Each controller_test function that interacts with the database will require an `AppState` instance in
+// order to communicate with the database.
+pub struct AppState<'a> {
+    pub connections: Mutex<u32>,
+    pub context: Arc<Database<'a>>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
+use std::sync::{Arc, Mutex};
 use actix_web::{web, App, HttpServer};
+use basic_web_template::AppState;
 use basic_web_template::config::Config;
+use basic_web_template::dao::Database;
 
 #[actix_web::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
+    println!("Start Server");
+    // Config
     let config_path: &'static str = "config.json";
     let config = Config::from_file(config_path);
     println!("Using configuration file from {0}", config_path);
+
+
+    // Connect to the database
+    let db_context = Database::new(&config.get_database_url()).await;
+    println!("Connected to database: {0}", config.get_database_url());
+
+    // Instantiate the app_state. This application state will be cloned for each Actix thread but
+    // the Arc of the DbContext will be reused in each Actix thread.
+    let app_state = web::Data::new(AppState {
+        connections: Mutex::new(0),
+        context: Arc::new(db_context),
+    });
+
+    // Start the web application
+    let app = HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+    })
+        .bind(config.get_app_url())?;
+    println!("listening on: {0}", config.get_app_url());
+
+    app.run().await
 }
